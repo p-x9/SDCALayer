07a731bb25c1ef8d0c83ccdb190ae119bd37f663
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-11-23 17:47:45 +0900

    implemented websocket hotrload example

---
+++ Example/calayer-ws/calayer-wsTests/calayer_wsTests.swift
@@ -0,0 +1,37 @@
+//
+//  calayer_wsTests.swift
+//  calayer-ws
+//
+//  Created by p-x9 on 2022/11/16.
+//  
+//
+
+import XCTest
+@testable import calayer_ws
+
+final class calayer_wsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
