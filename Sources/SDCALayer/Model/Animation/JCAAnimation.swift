00bd031edc87e591d981d2b42a51645a730f529f
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-04-07 01:19:52 +0900

    make `CAAnimation` conform to `IndirectlyCodable`

---
+++ Sources/SDCALayer/Model/Animation/JCAAnimation.swift
@@ -0,0 +1,51 @@
+//
+//  JCAAnimation.swift
+//
+//
+//  Created by p-x9 on 2024/04/06.
+//  
+//
+
+import Foundation
+import QuartzCore
+import KeyPathValue
+import IndirectlyCodable
+
+open class JCAAnimation: IndirectlyCodableModel, Codable {
+    public typealias Target = CAAnimation
+
+    public static var targetTypeName: String {
+        String(reflecting: Target.self)
+    }
+
+    static private let propertyMap: PropertyMap<CAAnimation, JCAAnimation> = .init([
+        .init(\.timingFunction, \.timingFunction),
+        .init(\.isRemovedOnCompletion, \.isRemovedOnCompletion)
+    ])
+
+    public var timingFunction: JCAMediaTimingFunction?
+    public var isRemovedOnCompletion: Bool?
+
+    public init() {}
+
+    public required convenience init(with object: Target) {
+        self.init()
+        applyProperties(with: object)
+    }
+
+    open func applyProperties(to target: Target) {
+        Self.propertyMap.apply(to: target, from: self)
+    }
+
+    open func applyProperties(with target: Target) {
+        Self.propertyMap.apply(to: self, from: target)
+    }
+
+    public func converted() -> Target? {
+        let animation = CAAnimation()
+
+        self.applyProperties(to: animation)
+        return animation
+    }
+}
+
